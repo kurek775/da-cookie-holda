b7ea066c37eb6d0fa9825ec03d3bfa93050b900c
Pavel Kurek <pavel.kurek@ecodef.cz>
2024-10-20 13:32:05 +0200

    DONE - v1

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,44 @@
-let globalCookies = [];
-
-function getCookiesFromTab(tab) {
-    chrome.cookies.getAll({ url: tab.url }, (cookies) => {
-        console.log(`Cookies from tab ${tab.id}:`, cookies);
-        globalCookies = globalCookies.concat(cookies);
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            func: (cookies) => {
-                if (cookies.length) {
-                    cookies.forEach((cookie) => {
-                        document.cookie = `${cookie.name}=${cookie.value}; path=/; domain=${cookie.domain}; SameSite=None; Secure;`;
-                    });
-                }
-            },
-            args: [cookies] 
+function getLtpaToken2(tab, callback) {
+    chrome.cookies.get({ url: tab.url, name: 'LtpaToken2' }, (cookie) => {
+        if (cookie) {
+            console.log('LtpaToken2 found:', cookie);
+            if (typeof callback === 'function') {
+                callback(cookie); 
+            }
+        } else {
+            console.error('LtpaToken2 not found in current tab.');
+        }
+    });
+}
+function setLtpaToken2ForLocalhost(cookie) {
+    chrome.tabs.query({}, (tabs) => {
+        tabs.forEach((tab) => {
+            if (tab.url && tab.url.includes('http://localhost:4200')) {
+                chrome.cookies.set({
+                    url: 'http://localhost:4200',
+                    name: cookie.name,
+                    value: cookie.value,
+                    path: cookie.path || '/',
+                    domain: 'localhost',
+                    secure: cookie.secure || false,
+                    httpOnly: cookie.httpOnly || false,
+                    expirationDate: cookie.expirationDate || (Date.now() / 1000 + 3600), // Defaults to 1 hour if not set
+                    sameSite: cookie.sameSite || 'Lax',
+                }, (setCookie) => {
+                    if (chrome.runtime.lastError) {
+                        console.error(`Error setting LtpaToken2 for localhost:4200: ${chrome.runtime.lastError}`);
+                    } else {
+                        console.log(`LtpaToken2 set for localhost:4200:`, setCookie);
+                    }
+                });
+            }
         });
     });
 }
 
+
 chrome.action.onClicked.addListener((currentTab) => {
-    chrome.tabs.query({}, (tabs) => {
-        tabs.forEach((tab) => {
-            if (!tab.active) {
-                getCookiesFromTab(tab);
-            }
-        });
+    getLtpaToken2(currentTab, (ltpaToken2Cookie) => {
+        setLtpaToken2ForLocalhost(ltpaToken2Cookie);
     });
 });
